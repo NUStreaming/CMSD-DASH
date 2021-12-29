a03454a2c5ac44bddd23855eb99e7cb9ced1262a
May Lim <mllim1719@gmail.com>
2021-12-29 08:49:33 +0800

    Update cmsdServer w metric logging and added cmsd header

--- server/nginx/cmsd_njs.js
+++ server/nginx/cmsd_njs.js
@@ -2,6 +2,7 @@ var querystring = require('querystring');
 var fs = require('fs');
 
 var LOGFILE = '/tmp/cmsd.log';  // most other directories wont work due to write permission
+var CSVFILE = '/tmp/cmsd.csv';  // most other directories wont work due to write permission
 var CONFIGFILE = '/tmp/cmsd_config.json';
 
 function writeLog(msg) {
@@ -10,7 +11,31 @@ function writeLog(msg) {
     try {
         fs.appendFileSync(LOGFILE, logLine);
     } catch (e) {
-        // unable to write log
+        // unable to write to file
+    }
+}
+
+function writeCsv(metricsObj) {
+    // One header and value row for each metricsObj for now
+    var csvHeaders = 'timestamp, '
+    var timestamp = Math.floor(new Date() / 1000);
+    var csvLine = timestamp + ', ';
+
+    for (const key in metricsObj) {
+        if (metricsObj.hasOwnProperty(key)) {
+            csvHeaders += (key + ', ')
+            csvLine += (metricsObj[key] + ', ')
+        }
+    }
+
+    csvHeaders += '\n'
+    csvLine += '\n'
+
+    try {
+        fs.appendFileSync(CSVFILE, csvHeaders);
+        fs.appendFileSync(CSVFILE, csvLine);
+    } catch (e) {
+        // unable to write to file
     }
 }
 
@@ -90,8 +115,12 @@ function getResourceUsingSubrequestBBRD(r) {
 
     writeLog('.. dashObjUri: ' + dashObjUri)
     writeLog('.. cmcdArgs: ' + cmcdArgs)
+    writeLog('.. r.variables.bufferBasedDelay: ' + r.variables.bufferBasedDelay)
     
     function done(res) {
+        r.headersOut['CMSD-Dynamic'] = ('com.example.dl=' + r.variables.bufferBasedDelay);
+        r.headersOut['Access-Control-Expose-Headers'] = ['CMSD-Dynamic'];
+
         r.return(res.status, res.responseBody);
     }
 
@@ -106,9 +135,11 @@ function getResourceUsingSubrequestBBRD(r) {
 // Triggered via bufferBasedResponseDelay.echo_sleep setting in nginx.conf
 //
 // Test queries -
-// curl http://localhost:8080/cmsd-njs/bufferBasedResponseDelay/media/vod/bbb_30fps_akamai/bbb_30fps.mpd?CMCD=bl%3D21300%2Ccom.example-bmx%3D20000%2Ccom.example-bmn%3D5000%2Cot%3Dv
+// curl -i http://localhost:8080/cmsd-njs/bufferBasedResponseDelay/media/vod/bbb_30fps_akamai/bbb_30fps.mpd?CMCD=bl%3D21300%2Ccom.example-bmx%3D20000%2Ccom.example-bmn%3D5000%2Cot%3Dv%2Cbr%3D1000%2Cd%3D4000%2Cmtp%3D1000
 //
 function getBufferBasedDelay(r) {
+    var metricsObj = {}
+
     writeLog('');
     writeLog('### getBufferBasedDelay() triggered!');
     var paramsObj = processQueryArgs(r);
@@ -130,24 +161,45 @@ function getBufferBasedDelay(r) {
     var bMin = Number(paramsObj['com.example-bmn']);
     var bMax = Number(paramsObj['com.example-bmx']);
     var bufferLength = Number(paramsObj['bl']);
+    
     writeLog('.. bMin = ' + bMin + '.. bMax = ' + bMax + ', bl = ' + bufferLength);
+    metricsObj['bufferMin'] = bMin
+    metricsObj['bufferMax'] = bMax
+    metricsObj['bufferLength'] = bufferLength
     
     var nextBitrate = Number(paramsObj['br']);
     var segDuration = Number(paramsObj['d']) / 1000;    // convert ms to s
     var measuredTput = Number(paramsObj['mtp']);
+
     writeLog('.. nextBitrate = ' + nextBitrate + ', segDuration = ' + segDuration + ', measuredTput = ' + measuredTput);
+    metricsObj['nextBitrate'] = nextBitrate
+    metricsObj['segDuration'] = segDuration
+    metricsObj['measuredTput'] = measuredTput
 
     // Retrieve $latestDelay in nginx.conf and compute delay to be applied
     var latestDelay = readConfig('latestDelay')
     if (latestDelay == null)    latestDelay = 0
     var latestDelayTimestamp = readConfig('latestDelayTimestamp')
     if (latestDelayTimestamp == null)    latestDelayTimestamp = 0
+
     writeLog('.. latestDelay = ' + latestDelay + 's, latestDelayTimestamp = ' + latestDelayTimestamp + 's');
+    metricsObj['latestDelay'] = latestDelay
+    metricsObj['latestDelayTimestamp'] = latestDelayTimestamp
 
     // Compute current delay after taking into account time passed
     var currentTimestamp = Math.round(new Date().getTime() / 1000);
-    var timePassed = currentTimestamp - latestDelayTimestamp
-    var currentDelay = Math.max(0, (latestDelay - timePassed))
+    var timePassed = currentTimestamp - latestDelayTimestamp;
+    var currentDelay = Math.max(0, (latestDelay - timePassed));
+
+    writeLog('.. currentDelay = ' + currentDelay + 's');
+    metricsObj['currentDelay'] = currentDelay
+
+    var expectedSegDownloadTime = (nextBitrate * segDuration) / measuredTput;
+    var expectedBufferLengthAfterDelayedDownload =  bufferLength - expectedSegDownloadTime - currentDelay;
+
+    writeLog('.. expectedSegDownloadTime = ' + expectedSegDownloadTime + 's, expectedBufferLengthAfterDelayedDownload = ' + expectedBufferLengthAfterDelayedDownload + 's');
+    metricsObj['expectedSegDownloadTime'] = expectedSegDownloadTime
+    metricsObj['expectedBufferLengthAfterDelayedDownload'] = expectedBufferLengthAfterDelayedDownload
     
     //
     // Case 1: Client is critical; update $latestDelay in nginx.conf and return delay=0 for this client
@@ -155,9 +207,11 @@ function getBufferBasedDelay(r) {
     //
     if (bufferLength < bMin) {
         writeLog('[case1] Critical client found !!');
+        metricsObj['case'] = '1-critical'
         
-        var segSize = nextBitrate * segDuration;
-        var newDelay = segSize / measuredTput;  // computed as expected segment download duration
+        // var segSize = nextBitrate * segDuration;
+        // var newDelay = segSize / measuredTput;  // computed as expected segment download duration
+        var newDelay = expectedSegDownloadTime;
 
         writeLog('.. newDelay = ' + newDelay + ', currentDelay = ' + currentDelay);
         if (newDelay > currentDelay) {  // update $latestDelay
@@ -165,9 +219,12 @@ function getBufferBasedDelay(r) {
             writeConfig('latestDelayTimestamp', currentTimestamp);
 
             writeLog('.. updated $latestDelay = ' + newDelay);
+            metricsObj['delayUpdateToConfig'] = newDelay
+            metricsObj['delayTimestampUpdateToConfig'] = currentTimestamp
         }
         else {
             writeLog('.. no update to $latestDelay')
+            metricsObj['delayUpdateToConfig'] = -1
         }
 
         delay = 0   // impt to set this critical client's request to delay=0
@@ -177,17 +234,31 @@ function getBufferBasedDelay(r) {
     // Case 2: Client is in surplus; apply delay
     //
     else if (bufferLength > bMax) {
-        writeLog('[case2] Surplus client found')
+        writeLog('[case2] Surplus client found, bufferLength: ' + bufferLength);
+        metricsObj['case'] = '2-surplus'
+        metricsObj['delayUpdateToConfig'] = -1
+    // else if (expectedBufferLengthAfterDelayedDownload > bMax) {
+    //     writeLog('[case2] Surplus client found, expectedBufferLengthAfterDelayedDownload: ' + expectedBufferLengthAfterDelayedDownload);
         delay = currentDelay;
     }
 
-    // Case 3: All other (normal) clients; apply delay
+    // Case 3: All other (normal) clients [bMin, bMax]; apply delay
     else {
-        writeLog('[case3] Normal client found')
-        delay = 0.5 * currentDelay;
+        writeLog('[case3] Normal client found, bufferLength: ' + bufferLength);
+        metricsObj['case'] = '3-normal'
+        metricsObj['delayUpdateToConfig'] = -1
+    // else if (expectedBufferLengthAfterDelayedDownload >= bMin) {
+    //     writeLog('[case3] Normal client found, expectedBufferLengthAfterDelayedDownload: ' + expectedBufferLengthAfterDelayedDownload);
+        // delay = 0.5 * currentDelay;
+        
+        var bRange = bMax - bMin;
+        delay = Math.round((((bufferLength - bMin) / bRange) * currentDelay), 2);
     }
     
     writeLog('Serving current client with delay = ' + delay + ' s!');
+    metricsObj['delayForThisReq'] = delay
+
+    writeCsv(metricsObj);
     return delay;
 }
 
